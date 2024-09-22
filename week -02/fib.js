78a8220060395ec3b1c98c17f09dce48da9f2ad2
yujou <93371864+110306041@users.noreply.github.com>
2024-09-22 15:16:49 +0800

    add comment

--- week -02/fib.js
+++ week -02/fib.js
@@ -1,7 +1,7 @@
 function fib(n) {
-  if (n < 2) return n;
-  if (n < 3) return 1;
-  else return fib(n - 1) + fib(n - 2);
+  if (n < 2) return n; //注意這邊的n是從0開始，所以0和1都會返回自己
+  if (n < 3) return 1; //n = 2的話是1
+  else return fib(n - 1) + fib(n - 2); //剩餘的遵循遞迴規則
 }
 
 console.log(fib(0)); // 0
