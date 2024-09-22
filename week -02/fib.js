6b1f3c10d77559152915ea411d380a9519492ad9
yujou <93371864+110306041@users.noreply.github.com>
2024-09-22 11:08:25 +0800

    Update fib.js

--- week -02/fib.js
+++ week -02/fib.js
@@ -1,6 +1,10 @@
-...
-function fib(n){
-  if(n==1|n==2) return 1;
-  else return fib(n-1)+fib(n-2);
+function fib(n) {
+  if (n < 2) return n;
+  if (n < 3) return 1;
+  else return fib(n - 1) + fib(n - 2);
 }
-...
+
+console.log(fib(0)); // 0
+console.log(fib(1)); // 1
+console.log(fib(5)); // 5
+console.log(fib(10)); // 55
