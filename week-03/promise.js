475ad06a3208ba52ffd4938fc1d281adb58d7f5b
yujou <93371864+110306041@users.noreply.github.com>
2024-09-28 22:47:17 +0800

    Update promise.js

--- week-03/promise.js
+++ week-03/promise.js
@@ -1,3 +1,6 @@
+//Promise 代表一個可能在未來某個時刻完成的操作，允許你在異步操作完成後執行後續操作，從而避免回調地獄（callback hell）的問題。
+//可以使用 .then() 方法來處理成功的結果，並使用 .catch() 方法來捕捉錯誤。
+
 function doJob(job, time) {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -9,6 +12,10 @@ function doJob(job, time) {
 
 let now = new Date();
 console.log(`開始工作 at ${now.toISOString()}`);
+
+//呼叫 doJob：先 doJob，傳入工作名稱「刷牙」和 1000 毫秒的時間。結束後會返回一個 Promise。
+//接著使用 .then() 方法來處理 Promise 的結果。
+//當「刷牙」工作完成後，將輸出結果「完成刷牙」並呼叫下一個 doJob，傳入「吃早餐」和 3000 毫秒。以此類推。
 
 doJob("刷牙", 1000)
   .then((result) => {
@@ -29,3 +36,4 @@ doJob("刷牙", 1000)
   .catch((error) => {
     console.error("發生錯誤:", error);
   });
+//在最後使用 .catch() 方法來捕捉任何可能發生的錯誤。如果在執行任何 doJob 時發生錯誤，則會進入這個 catch 區塊，並print出錯誤訊息。
